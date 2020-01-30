c836c56175ad4248baa37054c69d16a2e66fdcbf
itggot-fabian-beskow <fabian.beskow@elev.ga.ntig.se>
2020-01-30 15:31:03 +0100

    Utvecklade min databas och försökte göra lite restful routes

--- app.rb
+++ app.rb
@@ -3,18 +3,56 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
-enable :session
+enable :sessions
 
 get '/' do
   slim(:index)
 end
 
 get '/error' do
-  'ERROR'
+  if session[:error_message] != nil
+    error_message = session[:error_message]
+    session[:error_message] = nil
+    return error_message
+  else 
+    return 'Undefined Error'
+  end
 end
 
 get '/stocks' do
-  'STONKS'
+  if session[:userid] == nil
+    session[:error_message] = 'Not logged in'
+    redirect('/error')
+  end
+
+  db = SQLite3::Database.new('db/stonks.db')
+  db.results_as_hash = true
+
+  results = db.execute('SELECT * FROM stocks')
+
+  slim(:'stock/index', locals:{stock_list:results})
+end
+
+get '/my_page' do
+  if session[:userid] == nil
+    session[:error_message] = 'Not logged in'
+    redirect('/error')
+  end
+
+  db = SQLite3::Database.new('db/stonks.db')
+  db.results_as_hash = true
+
+  user_stock_relation = db.execute('SELECT * FROM relation_user_stock WHERE userid = ?', session[:userid])
+
+  user_stocks = [{}]
+
+  user_stock_relation.each_with_index do |relation, index|
+    stock = db.execute('SELECT * FROM stocks WHERE stockid = ?', relation['stockid']) 
+    user_stocks[index]['stockname'] = stock[0]['stockname']
+    user_stocks[index]['amount'] = relation['amount']
+  end
+
+  slim(:'user/show', locals:{user_stocks:user_stocks})
 end
 
 post '/login' do
@@ -24,15 +62,21 @@ post '/login' do
   db = SQLite3::Database.new('db/stonks.db')
   db.results_as_hash = true
 
-  results = db.execute('SELECT password_digest FROM users WHERE username = ?', username)
+  results = db.execute('SELECT * FROM users WHERE username = ?', username)
   p results
   if results == [] 
+    session[:error_message] = 'No user with that username'
     redirect('/error')
   end
+
   password_digest = results[0]['password_digest']
+  userid = results[0]['userid']
+
   if BCrypt::Password.new(password_digest) == password 
+    session[:userid] = userid
     redirect('/stocks')
   else
+    session[:error_message] = 'Wrong password'
     redirect('/error')
   end
 
--- projektplan.md
+++ projektplan.md
@@ -1,6 +1,9 @@
 # Projektplan
 
 ## 1. Projektbeskrivning (Beskriv vad sidan ska kunna göra)
+Sidan ska vara ett sätt för personer att köpa och sälja digitala aktier med "fantasi pengar". Man ska kunna lägga ut aktier för ett visst pris som andra användare sedan kan köpa dem för. Det ska även finnas administratörer som kan skapa pengar och aktier för att hålla marknaden vid liv.
+
 ## 2. Vyer (visa bildskisser på dina sidor)
 ## 3. Databas med ER-diagram (Bild)
-## 4. Arkitektur (Beskriv filer och mappar - vad gör/inehåller de?)
+
+## 4. Arkitektur (Beskriv filer och mappar - vad gör/innehåller de?)
